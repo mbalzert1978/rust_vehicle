1e44300feedcee10780d249069aaa5a144843300
Markus Iorio <markus.iorio@bfd.de>
2024-04-30 15:50:33 +0200

    logging

---
+++ src/logging.rs
@@ -0,0 +1,17 @@
+use crate::prelude::*;
+use std::{fs::File, sync::Arc};
+
+use tracing_subscriber::{fmt::layer, layer::SubscriberExt, Registry};
+pub fn init_tracing(file: File) -> Result<()> {
+    let stdout_log = layer().compact();
+    let json_layer = tracing_subscriber::fmt::layer()
+        .json()
+        .with_writer(Arc::new(file));
+    let subscriber = Registry::default().with(stdout_log).with(json_layer);
+
+    tracing::subscriber::set_global_default(subscriber).map_err(|e| Error::GenericError {
+        message: e.to_string(),
+    })?;
+
+    Ok(())
+}
