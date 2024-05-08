e084e38fe0965f4411fc95e7aaaa83e196a3e8a2
Markus Iorio <mbalzert@gmail.com>
2024-05-08 10:03:37 +0200

    using error fn

--- src/error.rs
+++ src/error.rs
@@ -18,9 +18,9 @@ pub enum Error {
     RunTime { message: String },
 
     // HTTP Errors
-    NotAllowed,
-    NotFound,
-    BadRequest { detail: String },
+    NotAllowed { message: String },
+    NotFound { message: String },
+    BadRequest { message: String },
 }
 
 impl core::fmt::Display for Error {
@@ -29,13 +29,41 @@ impl core::fmt::Display for Error {
     }
 }
 
+impl Error {
+    pub fn not_found<E: ToString>(error: E) -> Self {
+        Error::NotFound {
+            message: error.to_string(),
+        }
+    }
+    pub fn bad_request<E: ToString>(error: E) -> Self {
+        Error::BadRequest {
+            message: error.to_string(),
+        }
+    }
+    pub fn not_allowed<E: ToString>(error: E) -> Self {
+        Error::NotAllowed {
+            message: error.to_string(),
+        }
+    }
+    pub fn generic<E: ToString>(error: E) -> Self {
+        Error::Generic {
+            message: error.to_string(),
+        }
+    }
+    pub fn io<E: ToString>(error: E) -> Self {
+        Error::IO {
+            message: error.to_string(),
+        }
+    }
+}
+
 impl std::error::Error for Error {}
 
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
         let status = match self {
-            Error::NotAllowed => StatusCode::METHOD_NOT_ALLOWED,
-            Error::NotFound => StatusCode::NOT_FOUND,
+            Error::NotAllowed { .. } => StatusCode::METHOD_NOT_ALLOWED,
+            Error::NotFound { .. } => StatusCode::NOT_FOUND,
             Error::BadRequest { .. } => StatusCode::BAD_REQUEST,
             _ => StatusCode::INTERNAL_SERVER_ERROR,
         };
