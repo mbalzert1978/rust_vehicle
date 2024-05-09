23e5612be0895a96e973fe73d0f0ee898ebd1c47
Markus Iorio <mbalzert@gmail.com>
2024-05-09 18:58:48 +0200

    service skeleton

--- src/vehicles/services.rs
+++ src/vehicles/services.rs
@@ -1,1 +1,33 @@
+use crate::prelude::*;
 
+pub(crate) async fn insert(
+    ctx: crate::ctx::ApiContext,
+    payload: super::schemas::CreateVehicle,
+) -> Result<super::schemas::Vehicle> {
+    todo!()
+}
+
+pub(crate) async fn get_by_id(
+    ctx: crate::ctx::ApiContext,
+    id: uuid::Uuid,
+) -> Result<Option<super::schemas::Vehicle>> {
+    todo!()
+}
+
+pub(crate) async fn get_all(
+    ctx: crate::ctx::ApiContext,
+) -> Result<Vec<Option<super::schemas::Vehicle>>> {
+    todo!()
+}
+
+pub(crate) async fn update(
+    ctx: crate::ctx::ApiContext,
+    id: uuid::Uuid,
+    payload: super::schemas::UpdateVehicle,
+) -> Result<super::schemas::Vehicle> {
+    todo!()
+}
+
+pub(crate) async fn delete_by_id(ctx: crate::ctx::ApiContext, id: uuid::Uuid) -> Result<()> {
+    todo!()
+}
