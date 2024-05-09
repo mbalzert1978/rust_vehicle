bc5d1fc09945ec4a83050b08240dfd6f61b603a0
Markus Iorio <mbalzert@gmail.com>
2024-05-09 17:30:22 +0200

    service

--- src/health/services.rs
+++ src/health/services.rs
@@ -1,1 +1,11 @@
+use super::*;
+use crate::prelude::*;
 
+pub async fn get_database_status(
+    pool: &sqlx::Pool<sqlx::Postgres>,
+) -> Result<sqlx::postgres::PgQueryResult> {
+    sqlx::query(constants::QUERY)
+        .execute(pool)
+        .await
+        .map_err(Error::generic)
+}
