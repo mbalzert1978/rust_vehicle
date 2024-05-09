b07184c70db97ff8092467b49b5651bcf80c4418
Markus Iorio <mbalzert@gmail.com>
2024-05-09 21:01:55 +0200

    using super import

--- src/vehicles/services.rs
+++ src/vehicles/services.rs
@@ -1,23 +1,22 @@
+use super::*;
 use crate::prelude::*;
 
 pub(crate) async fn insert(
     ctx: crate::ctx::ApiContext,
-    payload: super::schemas::CreateVehicle,
-) -> Result<super::schemas::Vehicle> {
+    payload: schemas::CreateVehicle,
+) -> Result<schemas::Vehicle> {
     todo!()
 }
 
 pub(crate) async fn get_by_id(
     ctx: crate::ctx::ApiContext,
     id: uuid::Uuid,
-) -> Result<Option<super::schemas::Vehicle>> {
+) -> Result<Option<schemas::Vehicle>> {
     todo!()
 }
 
-pub(crate) async fn get_all(
-    ctx: crate::ctx::ApiContext,
-) -> Result<Vec<Option<super::schemas::Vehicle>>> {
-    let vehicle = sqlx::query_as!(super::schemas::Vehicle, r#"SELECT * FROM vehicles"#)
+pub(crate) async fn get_all(ctx: crate::ctx::ApiContext) -> Result<Vec<Option<schemas::Vehicle>>> {
+    let vehicle = sqlx::query_as!(schemas::Vehicle, r#"SELECT * FROM vehicles"#)
         .fetch_all(ctx.db.as_ref())
         .await
         .map_err(Error::generic);
@@ -26,8 +25,8 @@ pub(crate) async fn get_all(
 pub(crate) async fn update(
     ctx: crate::ctx::ApiContext,
     id: uuid::Uuid,
-    payload: super::schemas::UpdateVehicle,
-) -> Result<super::schemas::Vehicle> {
+    payload: schemas::UpdateVehicle,
+) -> Result<schemas::Vehicle> {
     todo!()
 }
 
