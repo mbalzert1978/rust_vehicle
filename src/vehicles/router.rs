62cbc6a43a1cd0394f998e9068428416f9ffd6a4
Markus Iorio <mbalzert@gmail.com>
2024-05-09 22:46:48 +0200

    get post on vehicles work

--- src/vehicles/router.rs
+++ src/vehicles/router.rs
@@ -10,8 +10,8 @@ pub fn routes() -> axum::Router {
     axum::Router::new()
         .route("/", get(get_all))
         .route("/", post(insert))
-        .route("/{id}", put(update))
         .route("/{id}", get(get_by_id))
+        .route("/{id}", put(update))
         .route("/{id}", delete(delete_by_id))
 }
 
