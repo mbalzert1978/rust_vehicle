2563f6903e4634a2d2143fb6b9869799367e9446
Markus Iorio <mbalzert@gmail.com>
2024-05-09 22:27:05 +0200

    services

--- src/vehicles/services.rs
+++ src/vehicles/services.rs
@@ -5,21 +5,47 @@ pub(crate) async fn insert(
     ctx: crate::ctx::ApiContext,
     payload: schemas::CreateVehicle,
 ) -> Result<schemas::Vehicle> {
-    todo!()
+    sqlx::query_as!(
+        schemas::Vehicle,
+        "
+        INSERT INTO vehicles 
+        (name, manufacturer, manufacturing_year, is_driveable, body) 
+        VALUES 
+        ($1, $2, $3, $4, $5) 
+        RETURNING *;
+        ",
+        payload.name,
+        payload.manufacturer,
+        payload.manufacturing_year,
+        payload.is_driveable,
+        payload.body
+    )
+    .fetch_one(ctx.db.as_ref())
+    .await
+    .map_err(Error::generic)
 }
 
 pub(crate) async fn get_by_id(
     ctx: crate::ctx::ApiContext,
     id: uuid::Uuid,
 ) -> Result<Option<schemas::Vehicle>> {
-    todo!()
+    sqlx::query_as!(
+        schemas::Vehicle,
+        "
+        SELECT * FROM vehicles WHERE id = $1;
+        ",
+        id
+    )
+    .fetch_optional(ctx.db.as_ref())
+    .await
+    .map_err(Error::generic)
 }
 
-pub(crate) async fn get_all(ctx: crate::ctx::ApiContext) -> Result<Vec<Option<schemas::Vehicle>>> {
-    let vehicle = sqlx::query_as!(schemas::Vehicle, r#"SELECT * FROM vehicles"#)
+pub(crate) async fn get_all(ctx: crate::ctx::ApiContext) -> Result<Vec<schemas::Vehicle>> {
+    sqlx::query_as!(schemas::Vehicle, "SELECT * FROM vehicles;")
         .fetch_all(ctx.db.as_ref())
         .await
-        .map_err(Error::generic);
+        .map_err(Error::generic)
 }
 
 pub(crate) async fn update(
@@ -27,9 +53,40 @@ pub(crate) async fn update(
     id: uuid::Uuid,
     payload: schemas::UpdateVehicle,
 ) -> Result<schemas::Vehicle> {
-    todo!()
+    sqlx::query_as!(
+        schemas::Vehicle,
+        "
+        UPDATE vehicles 
+        SET name = $2, 
+        manufacturer = $3, 
+        manufacturing_year = $4, 
+        is_driveable = $5, 
+        body = $6
+        WHERE id = $1
+        RETURNING *;
+        ",
+        id,
+        payload.name,
+        payload.manufacturer,
+        payload.manufacturing_year,
+        payload.is_driveable,
+        payload.body
+    )
+    .fetch_one(ctx.db.as_ref())
+    .await
+    .map_err(Error::generic)
 }
 
 pub(crate) async fn delete_by_id(ctx: crate::ctx::ApiContext, id: uuid::Uuid) -> Result<()> {
-    todo!()
+    sqlx::query!(
+        "
+        DELETE FROM vehicles
+        WHERE id = $1;
+        ",
+        id
+    )
+    .execute(ctx.db.as_ref())
+    .await
+    .map_err(Error::generic)?;
+    Ok(())
 }
