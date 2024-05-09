5f3c0f4136ce193f9d3743c361cc51ffefa8bccc
Markus Iorio <mbalzert@gmail.com>
2024-05-09 20:04:25 +0200

    first draft service

--- src/vehicles/services.rs
+++ src/vehicles/services.rs
@@ -17,7 +17,10 @@ pub(crate) async fn get_by_id(
 pub(crate) async fn get_all(
     ctx: crate::ctx::ApiContext,
 ) -> Result<Vec<Option<super::schemas::Vehicle>>> {
-    todo!()
+    let vehicle = sqlx::query_as!(super::schemas::Vehicle, r#"SELECT * FROM vehicles"#)
+        .fetch_all(ctx.db.as_ref())
+        .await
+        .map_err(Error::generic);
 }
 
 pub(crate) async fn update(
