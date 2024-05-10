2645ef3341b7d80c834d5bdc795f42a8a0f96914
Markus Iorio <markus.iorio@bfd.de>
2024-05-10 11:59:10 +0200

    qualify

--- src/span.rs
+++ src/span.rs
@@ -1,5 +1,3 @@
-use tower_http::trace::MakeSpan;
-
 #[derive(Clone)]
 pub(crate) struct CidSpan;
 
@@ -9,7 +7,7 @@ impl CidSpan {
     }
 }
 
-impl<B> MakeSpan<B> for CidSpan {
+impl<B> tower_http::trace::MakeSpan<B> for CidSpan {
     fn make_span(&mut self, _request: &axum::http::Request<B>) -> tracing::Span {
         let cid = crate::utils::create_correlation_id();
         let span = tracing::span!(tracing::Level::TRACE, "api", correlation_id = %cid.to_string());
