f98c65b1194f0945272d631119df47c2fe752a07
Markus Iorio <markus.iorio@bfd.de>
2024-05-10 09:03:30 +0200

    uses into

--- src/health/services.rs
+++ src/health/services.rs
@@ -4,8 +4,5 @@ use crate::prelude::*;
 pub(crate) async fn get_database_status(
     pool: &sqlx::Pool<sqlx::Postgres>,
 ) -> Result<sqlx::postgres::PgQueryResult> {
-    sqlx::query(constants::QUERY)
-        .execute(pool)
-        .await
-        .map_err(Error::generic)
+    Ok(sqlx::query(constants::QUERY).execute(pool).await?)
 }
