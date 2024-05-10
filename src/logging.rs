bf1cc4444ea087cb9196de6223507bd0c6488801
Markus Iorio <markus.iorio@bfd.de>
2024-05-10 09:02:20 +0200

    uses into

--- src/logging.rs
+++ src/logging.rs
@@ -9,7 +9,7 @@ pub fn init_tracing(file: File) -> Result<()> {
         .with_writer(Arc::new(file));
     let subscriber = Registry::default().with(stdout_log).with(json_layer);
 
-    tracing::subscriber::set_global_default(subscriber).map_err(Error::generic)?;
+    tracing::subscriber::set_global_default(subscriber)?;
 
     Ok(())
 }
